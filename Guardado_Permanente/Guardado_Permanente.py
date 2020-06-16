a9641a218ac3b8fb40293b1ad713186f06036004
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-06-16 09:28:16 -0500

    Guardado Permanente

---
+++ Guardado_Permanente/Guardado_Permanente.py
@@ -0,0 +1,44 @@
+import pickle
+
+class Persona:
+	def __init__(self, nombre, genero, edad):
+		self.nombre=nombre
+		self.genero=genero
+		self.edad=edad
+		print("Se ha creado una nueva persona con el nombre de ", self.nombre)
+	def __str__(self):
+		return "{} {} {}".format(self.nombre, self.genero, self.edad)
+
+class ListaPersonas:
+	personas=[]
+	def __init__(self):
+		listaDePersonas=open("FicheroExterno", "ab+")
+		listaDePersonas.seek(0)
+		try:
+			self.personas = pickle.load(listaDePersonas)
+			print("Se cargaron {} personas del fichero externo".format(len(self.personas)))
+		except:
+			print("El fichero esta vacio")
+		finally:
+			listaDePersonas.close()
+			del(listaDePersonas)
+	def agregarPersonas(self, p):
+		self.personas.append(p)
+		self.guardarPersonasEnFicheroExterno()
+	def mostrarPersonas(self):
+		for p in self.personas:
+			print(p)
+	def guardarPersonasEnFicheroExterno(self):
+		listaDePersonas=open("FicheroExterno", "wb")
+		pickle.dump(self.personas, listaDePersonas)
+		listaDePersonas.close()
+		del(listaDePersonas)
+	def mostrarInfoFicheroExterno(self):
+		print("La informacion del fichero externo es la siguiente:")
+		for p in self.personas:
+			print(p)
+
+miLista=ListaPersonas()
+persona=Persona("Laura", "Femenino", 18)
+miLista.agregarPersonas(persona)
+miLista.mostrarInfoFicheroExterno()
