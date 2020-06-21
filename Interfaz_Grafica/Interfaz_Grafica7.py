60c4e7b07b5ad42c9b67c2bd315d24d14534b6da
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-06-21 14:55:51 -0500

    Interfaz Grafica Parte 6 - Boton Abrir Archivos

---
+++ Interfaz_Grafica/Interfaz_Grafica7.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+
+root=Tk()
+
+def abrirArchivo():
+	archivo=filedialog.askopenfilename(title="Abrir", initialdir="D:", filetypes=(("Archivos Comprimidos", "*.zip"), ("Archivo de Texto", "*.txt"), ("Todos los archivos", "*.*")))
+	print(archivo)
+
+Button(root, text="Abrir Archivo", command=abrirArchivo).pack()
+
+
+root.mainloop()
