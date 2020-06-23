fdde69b516bbcd0562e997ce1a855a9f0e334c0a
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-06-23 16:02:25 -0500

    Practica Guiada Parte 2 - CRUD, Funcionalidad

--- Practica_Guiada/CRUD_Aplicacion.py
+++ Practica_Guiada/CRUD_Aplicacion.py
@@ -6,25 +6,120 @@ root = Tk()
 root.resizable(False, False)
 root.config(bg="#DEDECB")
 
+
+#----------------------------FUNCIONES--------------
+def conexionBBDD():
+	miConexion = sqlite3.connect("Usuarios")
+	miCursor = miConexion.cursor()
+	try:
+		miCursor.execute('''
+			CREATE TABLE INFOUSER (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			user_name VARCHAR(50),
+			surname VARCHAR(50),
+			password VARCHAR(50),
+			address VARCHAR(50),
+			comments VARCHAR(100)
+			)
+			''')
+		messagebox.showinfo("BBDD", "La BBDD ha sido creada con exito")
+	except:
+		messagebox.showwarning("Atencion", "La BBDD ya existe en el directorio")
+
+def salirAplicacion():
+	valor = messagebox.askquestion("Salir", "¿Desea salir de la aplicación?")
+	if valor == "yes":
+		root.destroy()
+
+def limpiarRegistros():
+	miId.set("")
+	miNombre.set("")
+	miApellido.set("")
+	miContrasena.set("")
+	miDireccion.set("")
+	campoComentario.delete(1.0, END)
+
+def crear():
+	miConexion = sqlite3.connect("Usuarios")
+	miCursor = miConexion.cursor()
+	"""miCursor.execute("INSERT INTO INFOUSER VALUES(NULL, '" + miNombre.get() + 
+		"','" + miApellido.get() + 
+		"','" + miContrasena.get() +
+		"','" + miDireccion.get() + 
+		"','" + campoComentario.get(1.0, END) + 
+		"')") """
+	datos = miNombre.get(), miApellido.get(), miContrasena.get(), miDireccion.get(), campoComentario.get(1.0, END)
+	miCursor.execute("INSERT INTO INFOUSER VALUES(NULL,?,?,?,?,?)",(datos))
+	miConexion.commit()
+	messagebox.showinfo("BBDD", "Registro creado con exito")
+
+def listar():
+	miConexion = sqlite3.connect("Usuarios")
+	miCursor = miConexion.cursor()
+	miCursor.execute("SELECT * FROM INFOUSER WHERE id="+miId.get())
+	infoUsuario = miCursor.fetchall()
+	for usuario in infoUsuario:
+		miId.set(usuario[0])
+		miNombre.set(usuario[1])
+		miApellido.set(usuario[2])
+		miContrasena.set(usuario[3])
+		miDireccion.set(usuario[4])
+		campoComentario.insert(1.0, usuario[5])
+	miConexion.commit()
+	messagebox.showinfo("Usuario Listado", "Se ha listado el usuario con ID="+miId.get())
+
+def actualizar():
+	miConexion = sqlite3.connect("Usuarios")
+	miCursor = miConexion.cursor()
+	"""miCursor.execute("UPDATE INFOUSER SET user_name='" + miNombre.get() +
+		"', surname='" + miApellido.get() + 
+		"', password='" + miContrasena.get() +
+		"', address='" + miDireccion.get() +
+		"', comments='" + campoComentario.get(1.0, END) +
+		"' WHERE id=" + miId.get())"""
+	datos = miNombre.get(), miApellido.get(), miContrasena.get(), miDireccion.get(), campoComentario.get(1.0, END)
+	miCursor.execute('''
+		UPDATE INFOUSER SET 
+			user_name=?, 
+			surname=?, 
+			password=?, 
+			address=?, 
+			comments=? 
+		WHERE id=''' + miId.get(), (datos))
+	valor = messagebox.askokcancel("Actualizar Usuario", "¿Desea actualizar el usuario "+miId.get()+"?")
+	if valor==True:
+		messagebox.showinfo("Actualizar Usuario", "El usuario ha sido actualizado con exito")
+	miConexion.commit()
+
+def eliminar():
+	miConexion = sqlite3.connect("Usuarios")
+	miCursor = miConexion.cursor()
+	miCursor.execute("DELETE FROM INFOUSER WHERE id=" + miId.get())
+	valor = messagebox.askokcancel("Eliminar Usuario", "¿Desea eliminar el usuario "+miId.get()+"?")
+	if valor==True:
+		messagebox.showinfo("Eliminar Usuario", "El usuario ha sido eliminado con exito")
+	miConexion.commit()
+
+
 #-----------------------------MENU-------------------
 barraMenu = Menu(root)
 root.config(menu=barraMenu, width=500, height=300)
 
 bbddMenu = Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="BBDD", menu=bbddMenu)
-bbddMenu.add_command(label="Conectar a BBDD")
-bbddMenu.add_command(label="Salir")
+bbddMenu.add_command(label="Conectar a BBDD", command=conexionBBDD)
+bbddMenu.add_command(label="Salir", command=salirAplicacion)
 
 borrarMenu = Menu(barraMenu, tearoff=0)
-barraMenu.add_cascade(label="Borrar Registros", menu=borrarMenu)
-borrarMenu.add_command(label="Limpiar Registros")
+barraMenu.add_cascade(label="Limpiar Registros", menu=borrarMenu)
+borrarMenu.add_command(label="Limpiar Registros", command=limpiarRegistros)
 
 crudMenu = Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="CRUD", menu=crudMenu)
-crudMenu.add_command(label="Crear")
-crudMenu.add_command(label="Listar")
-crudMenu.add_command(label="Actualizar")
-crudMenu.add_command(label="Eliminar")
+crudMenu.add_command(label="Crear", command=crear)
+crudMenu.add_command(label="Listar", command=listar)
+crudMenu.add_command(label="Actualizar", command=actualizar)
+crudMenu.add_command(label="Eliminar", command=eliminar)
 
 acercaMenu = Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="Ayuda", menu=acercaMenu)
@@ -57,21 +152,27 @@ labelComentarios = Label(frame1, text="Comentarios:")
 labelComentarios.grid(row=5, column=0, sticky="e", padx=10, pady=10)
 
 			#----------------Campos de textos
-campoID = Entry(frame1)
+miId = StringVar()
+miNombre= StringVar()
+miApellido = StringVar()
+miContrasena = StringVar()
+miDireccion = StringVar()
+
+campoID = Entry(frame1, textvariable=miId)
 campoID.grid(row=0, column=1, padx=10, pady=10)
 campoID.config(fg="BLUE", justify="center")
 
-campoNombre = Entry(frame1)
+campoNombre = Entry(frame1, textvariable=miNombre)
 campoNombre.grid(row=1, column=1, padx=10, pady=10)
 
-campoApellido = Entry(frame1)
+campoApellido = Entry(frame1, textvariable=miApellido)
 campoApellido.grid(row=2, column=1, padx=10, pady=10)
 
-campoContrasena = Entry(frame1)
+campoContrasena = Entry(frame1, textvariable=miContrasena)
 campoContrasena.grid(row=3, column=1, padx=10, pady=10)
 campoContrasena.config(show="*")
 
-campoDireccion = Entry(frame1)
+campoDireccion = Entry(frame1, textvariable=miDireccion)
 campoDireccion.grid(row=4, column=1, padx=10, pady=10)
 
 campoComentario = Text(frame1, width=20, height=5)
@@ -87,19 +188,19 @@ frame2.pack(fill="both", expand="True")
 frame2.config(relief="ridge", bd=5, bg="#E3E3D9")
 
 
-botonCrear = Button(frame2, text="Crear")
+botonCrear = Button(frame2, text="Crear", command=crear)
 botonCrear.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 botonCrear.config(bg="#3CC72E", width=10, cursor="plus")
 
-botonListar = Button(frame2, text="Listar")
+botonListar = Button(frame2, text="Listar", command=listar)
 botonListar.grid(row=0, column=1, sticky="e", padx=10, pady=10)
 botonListar.config(width=10, cursor="heart")
 
-botonActualizar = Button(frame2, text="Actualizar")
+botonActualizar = Button(frame2, text="Actualizar", command=actualizar)
 botonActualizar.grid(row=0, column=2, sticky="e", padx=10, pady=10)
 botonActualizar.config(bg="#D7E146", width=10, cursor="exchange")
 
-botonBorrar = Button(frame2, text="Eliminar")
+botonBorrar = Button(frame2, text="Eliminar", command=eliminar)
 botonBorrar.grid(row=1, column=1, sticky="e", padx=10, pady=10)
 botonBorrar.config(bg="#F64B48", width=10, cursor="pirate")
 
