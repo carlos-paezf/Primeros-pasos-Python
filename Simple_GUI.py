10e864f45dfb73698044404a00514ef691390e4f
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-09 11:25:20 -0500

    Interfaces sencillas

---
+++ Simple_GUI.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.alert('Caja de alerta')
+
+pyautogui.confirm('Desea proceder?')
+
+pyautogui.confirm('Elija una opcion', buttons=['A', 'B', 'C'])
+
+pyautogui.password('Ingrese su contraseña')
+
+pyautogui.prompt('Cual es tu nombre')
